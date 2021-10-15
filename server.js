867cc9b4f575e1e31f0b2c5ec3459332d907da6e
amenasria <alexandre.menasria@centrale-marseille.fr>
2021-10-15 13:47:14 +0200

    Ajout du socket dans le server.js

--- server.js
+++ server.js
@@ -2,17 +2,36 @@
 const port = process.env.PORT || 8081
 
 
-const express = require('express')
+const express = require('express');
 const http = require('http');
+const crypto = require('crypto');
+
 
 const app = express()  // create server instance
 const server = http.createServer(app)
+const io = require('socket.io')(server)
 
-// app.use(express.static('front/dist'))
+app.use(express.static('front/dist'))
 
 app.get('/baguette', (req, res) => {
     res.json({username: 'Baguette'})
 })
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html', { root: __dirname })
+})
+
+// Etablissement de la connexion
+io.on('connection', (socket) =>{
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  console.log(`Connecté au client: socket_id = ${socket.id}`);
+  let salt = "Baguette";
+  let room_token = crypto.createHash('sha256').update(String(Date.now()) + salt).digest('hex').slice(0, 5).toUpperCase();
+  io.emit('get_hash', {room_token: room_token});
+})
+
 
 
 server.listen(port, () => console.log(`Listening on port ${port}`))

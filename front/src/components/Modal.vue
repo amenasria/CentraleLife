90214c4ef824d91c455288bd2d1b6bc80abfe767
Alexandre Menasria <alexandre.menasria@centrale-marseille.fr>
2021-10-18 15:55:10 +0200

    Join Room modal creation

---
+++ front/src/components/Modal.vue
@@ -0,0 +1,149 @@
+<template>
+  <transition name="fade">
+    <div class="modal" v-if="show">
+      <div class="modal-backdrop" @click="closeModal()"/>
+
+      <div class="modal-dialog">
+        <div class="modal-header">
+          <div class="close-btn-header">
+            <button type="button" class="modal-close" @click="closeModal()">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+                <path
+                  fill="currentColor"
+                  d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div class="title-header">
+            <slot name="header"/>
+          </div>
+        </div>
+
+        <div class="modal-body">
+          <slot name="body"/>
+        </div>
+
+        <div class="modal-footer">
+          <slot name="footer"/>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Modal", // Source: https://medium.com/notonlycss/how-to-build-a-modal-in-vue-js-b3db644afaeb
+  data() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    closeModal() {
+      this.show = false;
+      document.querySelector("body").classList.remove("overflow-hidden");
+    },
+    openModal() {
+      this.show = true;
+      document.querySelector("body").classList.add("overflow-hidden");
+    }
+  }
+};
+</script>
+
+
+<style>
+    .modal {
+        overflow-x: hidden;
+        overflow-y: auto;
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 9;
+        }
+
+    .modal-backdrop {
+        background-color: rgba(0, 0, 0, 0.3);
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 1;
+        }
+
+    .modal-dialog {
+        background-color: #ffffff;
+        position: relative;
+        width: 600px;
+        margin: 50px auto;
+        display: flex;
+        flex-direction: column;
+        border-radius: 5px;
+        z-index: 2;
+        }
+
+    @media screen and (max-width: 992px) {
+        .modal-dialog {
+            width: 90%;
+        }
+    }
+
+    .modal-close {
+        width: 25px;
+        border: 0;
+        background-color: transparent;
+        cursor: pointer;
+        margin: 0;
+    }
+
+    .modal-header {
+        padding: 10px 20px 0px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .title-header {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .title-header > h1 {
+      margin: 0 0 0.2em;
+    }
+
+    .close-btn-header {
+        display: flex;
+        flex-direction: row-reverse;
+
+    }
+
+    .modal-body {
+        padding: 0px 20px 10px;
+        overflow: auto;
+        display: flex;
+        flex-direction: column;
+        align-items: stretch;
+    }
+
+    .modal-footer {
+        margin-top: 10px;
+        padding: 15px 20px 15px;
+        background-color: #eff0f4;
+        border-radius: 0 0 5px 5px;
+    }
+
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity 0.2s;
+    }
+
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
+    }
+
+</style>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -2,42 +2,125 @@
   <div>
       <h1>Centrale Life 👽</h1>
       <div class="action">
-        <div id="rejoindre_salle" class="rejoindre_salle"><button>Je rejoins une salle !</button></div>
-        <div id="creer_salle" class="creer_salle"><button  :disabled="hasCreatedRoom" v-on:click="createRoom">Je créé ma salle !</button></div>
+        <Modal ref="RoomModal">
+          <template v-slot:header>
+            <h1>Rejoindre une salle</h1>
+          </template>
+          <template v-slot:body>
+            <span style="text-align: center; font-size: 15px; color: var(--color-input-ft)">Entre un lien d'invitation ci-dessous pour rejoindre un serveur existant</span>
+            <div style="margin: 3% 0 3% 0"><b>Lien d'invitation</b> <b style="color: red;">*</b></div>
+            <div>
+              <input type="text" id="lien_invitation" name="lien_invitation" v-model="lien_invitation" placeholder="https://localhost:8080/room/F5444">
+            </div>
+            <div style="margin: 3% 0 2% 0"><b>Les invitations devraient ressembler à</b></div>
+            <div style="color: var(--color-examples)">F5444<br/>https://localhost:8080/room/F5444
+            </div>
+          </template>
+          <template v-slot:footer>
+            <div style="display: flex; align-items: flex-start; justify-content: space-between;">
+              <button @click="$refs.RoomModal.closeModal()" class="back_btn">Retour</button>
+              <button v-on:click="joinRoom()" class="join_btn">Rejoindre la salle</button>
+            </div>
+          </template>
+        </Modal>
+        <div id="rejoindre_salle" class="rejoindre_salle" v-on:click="$refs.RoomModal.openModal()"><button>Je rejoins une salle !</button></div>
+        <div id="creer_salle" class="creer_salle"><button  :disabled="hasCreatedRoom" v-on:click="createRoom">Je créé ma salle !</button></div> <!-- TODO: Ajouter msg si serveur répond pas direct kwa -->
       </div>
   </div>
 </template>
 
 <script>
-
+import Modal from "../components/Modal.vue"
+import axios from 'axios';
  
-  export default {
-    name: 'Home',
-    data: function() {
-      return {
-        hasCreatedRoom: false
-      }
+export default {
+  name: 'Home',
+  components: {
+    Modal,
+  },
+  data: function() {
+    return {
+      hasCreatedRoom: false,
+      lien_invitation: ""
+    }
+  },
+  methods: {
+    createRoom: function () {
+      this.hasCreatedRoom = true;
+      const io = require("socket.io-client");
+      const socket = io("http://localhost:8081");
+      socket.on("connect", () => {
+          console.log(`Connecté au serveur: socket_id = ${socket.id}`);
+      });
+      socket.on('get_hash', function(msg){
+          console.log(msg["room_token"]);
+          window.location.replace("http://localhost:8080/room/" + msg["room_token"])
+      });
     },
-    methods: {
-      createRoom: function () {
-        this.hasCreatedRoom = true;
-        const io = require("socket.io-client");
-        const socket = io("http://localhost:8081");
-        socket.on("connect", () => {
-            console.log(`Connecté au serveur: socket_id = ${socket.id}`);
-        });
-        socket.on('get_hash', function(msg){
-            console.log(msg["room_token"]);
-            window.location.replace("http://localhost:8080/room/" + msg["room_token"])
-        });
-      },
 
+    joinRoom: function () {
+      let room_regexp = new RegExp("https?://(www.)?localhost:8081/room/[a-zA-Z0-9]{5}");
+
+      // If the link matches the model then check if the room exists in the DB
+      if (this.lien_invitation.match(room_regexp) != null) {
+        console.log(this.lien_invitation)
+        axios
+          .head(this.lien_invitation)
+          .then(response => console.log((this.info = response)))
+      }
     }
 
   }
+
+}
 </script>
 
 <style>
+    :root {
+        --bleu-centrale: #000f9f;
+        --color-input-bg: #e2eaf5;
+        --color-input-ft-placeholder:#9ca9bd;
+        --color-input-ft:#757f8d;
+        --color-case: #eff0f4;
+        --color-examples: #535a64;
+    }
+
+    #lien_invitation {
+      background-color: var(--color-input-bg);
+      color: var(--color-input-ft);
+      border: none;
+      line-height: 4ch;
+      width: 98%;
+      font-size: 18px;
+      padding-left: 2%;
+    }
+
+    #lien_invitation:focus {
+      outline: none
+    }
+
+    #lien_invitation::placeholder {
+      color: var(--color-input-ft-placeholder);
+    }
+
+  .join_btn {
+    background-color: var(--bleu-centrale);
+    border: 0;
+    color: white;
+    border-radius: 4px;
+    padding: 1em 1em;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .back_btn {
+    background-color: transparent;
+    border: 0;
+    font-weight: bold;
+    cursor: pointer;
+    padding: 1em 1em;
+    color: var(--color-input-ft);
+  }
   .action {
     background-color: azure;
     margin: 0 20vw;

be96db5ce1acd250abe44fb541cf720b98694f70
Marie Le Goué <marie.le_goue@centrale-marseille.fr>
2021-10-28 12:31:20 +0200

    remplacer tous les localhosts

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -48,18 +48,18 @@ export default {
     createRoom: function () {
       this.hasCreatedRoom = true;
       const io = require("socket.io-client");
-      const socket = io("http://localhost:8081");
+      const socket = io("http://node.marjolaine.ovh1.ec-m.fr");
       socket.on("connect", () => {
           console.log(`Connecté au serveur: socket_id = ${socket.id}`);
       });
       socket.on('get_hash', function(msg){
           console.log(msg["room_token"]);
-          window.location.replace("http://localhost:8080/room/" + msg["room_token"])
+          window.location.replace("http://node.marjolaine.ovh1.ec-m.fr/room/" + msg["room_token"])
       });
     },
 
     joinRoom: function () {
-      let room_regexp = new RegExp("/^https?://(www.)?localhost:8081/room/[a-zA-Z0-9]{5}$/");
+      let room_regexp = new RegExp("/^https?://(www.)?node.marjolaine.ovh1.ec-m.fr/room/[a-zA-Z0-9]{5}$/");
 
       // If the link matches the model then check if the room exists in the DB
       if (this.lien_invitation.match(room_regexp) != null) {
